57be619c61453f8de5f9d762d7393b9a29d3aba7
Abdullah Aydin <uAbdullah29@gmail.com>
2021-02-06 17:20:16 +0300

    anasayfaya duyurular bölümü eklendi

--- src/routes/Dashboard/Dashboard.js
+++ src/routes/Dashboard/Dashboard.js
@@ -1,57 +1,16 @@
 // components
 import Cards from "./components/Cards";
+import Announcement from "./components/Announcement";
 // ant design
-import { Card, Col, Row } from "antd";
+// import { Card, Col, Row } from "antd";
 //styles
 import "./Dashboard.css";
 
 function Dashboard() {
   return (
     <>
-      {/* AYLIK FATURA KISMI */}
       <Cards />
-
-      {/* ./ AYLIK FATURA KISMI */}
-      <div className="site-card-wrapper">
-        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-          <Col
-            xl={6}
-            lg={6}
-            md={24}
-            sm={24}
-            xs={24}
-            className="card_charts_noti"
-          >
-            <Card bordered={true} className="card">
-              <Col xl={24} lg={24} md={24} sm={24} xs={24}>
-                <h1>Duyurular</h1>
-              </Col>
-            </Card>
-          </Col>
-          <Col
-            xl={6}
-            lg={6}
-            md={24}
-            sm={24}
-            xs={24}
-            className="card_charts_noti"
-          >
-            <Card bordered={true} className="card">
-              <Col xl={24} lg={24} md={24} sm={24} xs={24}>
-                <h1>Duyurular</h1>
-              </Col>
-            </Card>
-          </Col>
-
-          {/* CUSTOMER REVIEW */}
-          {/* <Col xl={6} lg={6} md={24} sm={24} xs={24}>
-            <Card title={`${123123}`} bordered={true}>
-              Aylık Elektrik Faturası
-            </Card>
-          </Col> */}
-          {/* CUSTOMER REVIEW */}
-        </Row>
-      </div>
+      <Announcement />
     </>
   );
 }
